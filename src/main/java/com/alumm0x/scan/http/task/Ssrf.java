f7f18d548beb20e874d556031a2d4f0072f71583
alumm0x <zhoufeng9186@cvte.com>
2023-03-28 15:51:52 +0800

    ssrf新增dnslog验证

--- src/main/java/com/alumm0x/scan/http/task/Ssrf.java
+++ src/main/java/com/alumm0x/scan/http/task/Ssrf.java
@@ -1,12 +1,13 @@
 package com.alumm0x.scan.http.task;
 
+import burp.IBurpCollaboratorClientContext;
+import burp.IBurpCollaboratorInteraction;
 import burp.IHttpRequestResponse;
 import com.alumm0x.scan.LogEntry;
 import com.alumm0x.scan.http.task.impl.TaskImpl;
 import com.alumm0x.tree.UselessTreeNodeEntity;
 import com.alumm0x.util.BurpReqRespTools;
 import com.alumm0x.util.CommonStore;
-import com.alumm0x.util.SourceLoader;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
@@ -14,8 +15,8 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,6 +25,8 @@ public class Ssrf extends TaskImpl {
     public static String name = "Ssrf";
     public static String comments = "服务端的跨站请求检测。参数中是url的，将构造恶意地址，查看响应是否响应的地址。";
     public static String fix = "对于参数是url的，后端需要白名单限制。";
+    public boolean dnslog = false;
+    public IBurpCollaboratorClientContext collaboratorClientContext;
 
     public UselessTreeNodeEntity entity;
     public Ssrf(UselessTreeNodeEntity entity) {
@@ -32,12 +35,8 @@ public class Ssrf extends TaskImpl {
 
     @Override
     public void run() {
-        /**
-         * 检测逻辑
-         * 1、所有参数都添加特殊字符
-         * 2、然后检查响应是否不同或者存在关键字
-         * */
-
+        collaboratorClientContext = CommonStore.callbacks.createBurpCollaboratorClientContext();
+        String payload = collaboratorClientContext.generatePayload(true);
         String regex = "http[s]?://(.*?)[/&\"]+?\\w*?"; //分组获取域名
         String evilHost = "evil6666.com";
         //如果有body参数，需要多body参数进行测试
@@ -48,7 +47,7 @@ public class Ssrf extends TaskImpl {
             if (matcher.find()){//没匹配到则不进行后续验证
                 String domain = matcher.group(1);
                 // 修改为别的域名
-                String req_body = new String(BurpReqRespTools.getReqBody(entity.getRequestResponse())).replace(domain, evilHost);
+                String req_body = dnslog ? new String(BurpReqRespTools.getReqBody(entity.getRequestResponse())).replace(domain, payload) : new String(BurpReqRespTools.getReqBody(entity.getRequestResponse())).replace(domain, evilHost);
                 //新的请求包
                 CommonStore.okHttpRequester.send(BurpReqRespTools.getUrlWithOutQuery(entity.getRequestResponse()),
                         BurpReqRespTools.getMethod(entity.getRequestResponse()),
@@ -66,7 +65,7 @@ public class Ssrf extends TaskImpl {
                 String domain = matcher.group(1);
                 CommonStore.callbacks.printOutput(domain);
                 // 修改为别的域名
-                String req_query = BurpReqRespTools.getQuery(entity.getRequestResponse()).replace(domain, evilHost);
+                String req_query = dnslog ? BurpReqRespTools.getQuery(entity.getRequestResponse()).replace(domain, payload) : BurpReqRespTools.getQuery(entity.getRequestResponse()).replace(domain, evilHost);
                 //新的请求包
                 CommonStore.okHttpRequester.send(BurpReqRespTools.getUrlWithOutQuery(entity.getRequestResponse()),
                         BurpReqRespTools.getMethod(entity.getRequestResponse()),
@@ -81,7 +80,6 @@ public class Ssrf extends TaskImpl {
                     "##url: "+ BurpReqRespTools.getUrl(entity.getRequestResponse()));
         }
     }
-
 }
 
 class SsrfCallback implements Callback {
@@ -106,15 +104,46 @@ class SsrfCallback implements Callback {
         IHttpRequestResponse requestResponse = BurpReqRespTools.makeBurpReqRespFormOkhttp(call,response, BurpReqRespTools.getHttpService(entity.getRequestResponse()));
         logEntry.requestResponse = CommonStore.callbacks.saveBuffersToTempFiles(requestResponse);
         logEntry.Status = (short) response.code();
-        // 检查响应中是否存在flag
-        if (new String(BurpReqRespTools.getRespBody(entity.getRequestResponse())).contains("evil6666.com")) {
-            logEntry.hasVuln();
-        }else if (response.isSuccessful()){
-            logEntry.hasVuln();
-            logEntry.Comments = "并没有在响应中呈现，需要在使用dnslog的url确认是否会发起请求";
-        }else {
-            logEntry.onResponse();
+        if (((Ssrf)task).dnslog) {
+            logEntry.Comments = "try DnsLog";
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            List<IBurpCollaboratorInteraction> ret = ((Ssrf) task).collaboratorClientContext.fetchAllCollaboratorInteractions();
+            if (ret.size() > 0){
+                logEntry.hasVuln();
+                for (IBurpCollaboratorInteraction i :
+                        ret) {
+                    logEntry.Comments += i;
+                }
+            } else {
+                logEntry.onResponse();
+            }
+        } else {
+            // 检查响应中是否存在flag
+            if (new String(BurpReqRespTools.getRespBody(entity.getRequestResponse())).contains("evil6666.com")) {
+                logEntry.hasVuln();
+                logEntry.Comments = "in Resp.";
+                tryDnslog();
+            } else if (response.isSuccessful()) {
+                logEntry.hasVuln();
+                logEntry.Comments = "并没有在响应中呈现，需要在使用dnslog的url确认是否会发起请求";
+                tryDnslog();
+            } else {
+                logEntry.onResponse();
+            }
         }
         CommonStore.logModel.update();
+    }
+
+    /**
+     * 尝试dnslog，验证ssrf
+     */
+    private void tryDnslog(){
+        Ssrf ssrf = new Ssrf(this.entity);
+        ssrf.dnslog = true;
+        ssrf.run();
     }
 }
